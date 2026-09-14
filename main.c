126c9b1bd7da4ade6b0eb5416db2bca28d860b91
Nipuna Silva <nipunasilva2004@gmail.com>
2026-09-14 19:47:52 +0530

    Update hospital system

--- main.c
+++ main.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+#define NUM_SPECIALTIES 4
+
+
+
+/*Doctor Specialties*/
+const int    specialtyID[NUM_SPECIALTIES]   = {1, 2, 3, 4};
+const char   specialtyName[NUM_SPECIALTIES][30] = {
+    "General Practice (OPD)", "Paediatrics", "Cardiology", "Neurology"
+};
+const double baseFee[NUM_SPECIALTIES]      = {1500.00, 2500.00, 4500.00, 5000.00};
+const int    consultTime[NUM_SPECIALTIES]  = {15, 20, 30, 30};
+const int    dailyCap[NUM_SPECIALTIES]     = {30, 20, 12, 10};
+
 int main(void) {
     printf("Welcome to the Smart Hospital & Resource Allocation System\n");
     return 0;
