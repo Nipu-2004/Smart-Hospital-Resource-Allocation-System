2118c609b4ac912bc474472ea7a850ec66ca33cd
Nipuna Silva <nipunasilva2004@gmail.com>
2026-09-14 19:29:49 +0530

    Update C file

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main(void) {
+    printf("Welcome to the Smart Hospital & Resource Allocation System\n");
     return 0;
 }
